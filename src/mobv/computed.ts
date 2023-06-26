67eb8f35e8e04c4e8801b16b857ec932a6dc7123
黎贵奇 <carney520@hotmail.com>
2023-06-26 09:22:39 +0800

    feat: improve computed

--- src/mobv/computed.ts
+++ src/mobv/computed.ts
@@ -1,5 +1,7 @@
 /* eslint-disable prefer-rest-params */
-import { effectScope, unref, computed as vueComputed } from 'vue'
+import { Ref, effectScope, unref, computed as vueComputed } from 'vue'
+
+const COMPUTED_CACHE: unique symbol = Symbol('computed_cache')
 
 export function computed<This, Return, Value extends () => Return>(
   value: Value,
@@ -14,16 +16,26 @@ export function computed<This, Return, Value extends () => Return>(
   }
 
   context.addInitializer(function (this: unknown) {
-    const scope = effectScope(true)
-
-    const val = scope.run(() => vueComputed(() => value.call(this)))
-
-    Object.defineProperty(this, context.name, {
-      configurable: true,
-      enumerable: false,
-      get() {
-        return unref(val)
-      }
-    })
+    if (!Object.prototype.hasOwnProperty.call(this, COMPUTED_CACHE)) {
+      Object.defineProperty(this, COMPUTED_CACHE, {
+        configurable: true,
+        enumerable: false,
+        value: new Map()
+      })
+    }
   })
+
+  return function (this: Object) {
+    const cache = this[COMPUTED_CACHE] as Map<string | symbol, Ref<Return>>
+    if (!cache.has(context.name)) {
+      // 异步初始化
+      const scope = effectScope(true)
+
+      const val = scope.run(() => vueComputed(() => value.call(this)))!
+
+      cache.set(context.name, val)
+    }
+
+    return unref(cache.get(context.name))
+  } as Value
 }
